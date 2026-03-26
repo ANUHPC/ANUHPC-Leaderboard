deb73faa0e72f57317437293d3d512e2145014ee
Xeno-Tek <u7508601@anu.edu.au>
2026-03-26 21:11:08 +1100

    Clean slate: remove all old output data and unused benchmarks

--- output/HPL/Sithum/_OLD/nvidiaTest2 27/run_hpl_check_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script will be executed by each MPI rank
-echo "MPI Rank: $OMPI_COMM_WORLD_RANK, Local Rank: $OMPI_COMM_WORLD_LOCAL_RANK, CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-./xhpl
